ac2abd9294fe4844586baee4fe70de2098b0a492
yuan <yuanshuai091@gmail.com>
2016-07-11 22:02:44 +0800

    hello3

--- src/com/nowcoder/Main.java
+++ src/com/nowcoder/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("HELLO NOWCODER");
-        System.out.println("HELLO NOWCODER2");
+        System.out.print("HELLO NOWCODER2");
+        System.out.println("HELLO NOWCODER3");
     }
 }
